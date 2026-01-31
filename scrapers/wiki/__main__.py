da081f31a846a43b9da130f8588f49ba5ed55c3a
darciu <giemzadariusz@gmail.com>
2026-02-01 00:56:13 +0100

    upsert data in batches

--- scrapers/wiki/__main__.py
+++ scrapers/wiki/__main__.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import asyncio
 import pickle
-from scrapers.wiki.utils import get_latest_dumpstatus_url, fetch_dumpstatus, is_dump_done, get_download_urls, pair_wiki_files, get_unique_indices, read_multistream
+from scrapers.wiki.utils import get_latest_dumpstatus_url, fetch_dumpstatus, is_dump_done, get_download_urls, pair_wiki_files, get_unique_indices, multistream_to_mongodb
 from scrapers.wiki.async_func import run_scraper
+from backend.db.mongodb.connection import MongoManager
 import logging
 
 logging.basicConfig(
@@ -10,11 +11,16 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
-
+download_path = 'scrapers/wiki/downloaded_data/'
+MONGODB_URI = "mongodb://admin:pass123@localhost:27017/"
+mongodb_client = MongoManager(MONGODB_URI, "scraper_db")
 
 if __name__ == "__main__":
     logger.info('WIKI SCRAPER')
-    Path("data").mkdir(parents=True, exist_ok=True)
+    if not mongodb_client.is_healthy():
+        logger.critical('Could not establish connection with MongoDB')
+        exit(1)
+    Path(download_path).mkdir(parents=True, exist_ok=True)
     dumpstatus_url = get_latest_dumpstatus_url()
     dumpstatus = fetch_dumpstatus(dumpstatus_url)
     
@@ -22,14 +28,12 @@ if __name__ == "__main__":
     if not is_dump_done(articlesmultistreamdump):
         exit(1)
     download_urls = get_download_urls(articlesmultistreamdump)
-    print(download_urls)
-    asyncio.run(run_scraper(download_urls))
+    asyncio.run(run_scraper(download_urls, download_path))
 
-    index_multistream_pairs = pair_wiki_files("data")
+    index_multistream_pairs = pair_wiki_files(download_path)
 
     for pair in index_multistream_pairs:
-        indices = get_unique_indices("data/"+pair['index'])
-        all_pages = read_multistream("data/"+pair['multistream'], indices)
-        with open(f"{'temp/'+pair['multistream']}.pkl", "wb") as f:
-            pickle.dump(all_pages, f)
+        indices = get_unique_indices(download_path+pair['index'])
+        multistream_to_mongodb(mongodb_client, download_path+pair['multistream'], indices)
+
 
