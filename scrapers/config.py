ab53f8bc23b9a5c64ca0ab47f68393f100f037e6
darciu <giemzadariusz@gmail.com>
2026-02-05 18:47:31 +0100

    working dockerized scraper

---
+++ scrapers/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class WikiScraperSettings(BaseSettings):
+    MONGO_INITDB_ROOT_USERNAME: str
+    MONGO_INITDB_ROOT_PASSWORD: str
+    WIKI_DOWNLOAD_PATH: str = '/app/data/wiki_dumps/'
+    MONGO_PORT: int = 27017
+    RSS_URL: str = 'https://dumps.wikimedia.org/plwiki/latest/plwiki-latest-pages-articles-multistream-index.txt.bz2-rss.xml'
+    
+    @property
+    def mongodb_uri(self) -> str:
+        return (
+            f"mongodb://{self.MONGO_INITDB_ROOT_USERNAME}:{self.MONGO_INITDB_ROOT_PASSWORD}@mongodb:{27017}/?authSource=admin" # log as admin
+        )
+
+    # Load envs from .env file, get only relevant variables, variables are case sensitive
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore", case_sensitive=True)
--- scrapers/wiki/__main__.py
+++ scrapers/wiki/__main__.py
@@ -3,6 +3,7 @@ import asyncio
 import os
 from scrapers.wiki.utils import get_latest_dumpstatus_url, fetch_dumpstatus, is_dump_done, get_download_urls, pair_wiki_files, get_unique_indices, multistream_to_mongodb
 from scrapers.wiki.async_func import run_scraper
+from scrapers.config import WikiScraperSettings
 from backend.db.mongodb.connection import MongoManager
 import logging
 
@@ -11,10 +12,13 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
+
+wiki_scraper_settings = WikiScraperSettings()
+
 download_path = os.getenv('WIKI_DOWNLOAD_PATH', './data/wiki_dumps')
-MONGODB_URI = "mongodb://admin:pass123@localhost:27017/"
+MONGODB_URI = wiki_scraper_settings.mongodb_uri
 mongodb_client = MongoManager(MONGODB_URI, "scraper_db")
-rss_url = "https://dumps.wikimedia.org/plwiki/latest/plwiki-latest-pages-articles-multistream-index.txt.bz2-rss.xml"
+RSS_URL = wiki_scraper_settings.RSS_URL
 
 if __name__ == "__main__":
     logger.info('WIKI SCRAPER')
@@ -22,7 +26,7 @@ if __name__ == "__main__":
         logger.critical('Could not establish connection with MongoDB')
         exit(1)
     Path(download_path).mkdir(parents=True, exist_ok=True)
-    dumpstatus_url = get_latest_dumpstatus_url(rss_url)
+    dumpstatus_url = get_latest_dumpstatus_url(RSS_URL)
     dumpstatus = fetch_dumpstatus(dumpstatus_url)
     
     articlesmultistreamdump = dumpstatus.get('jobs',{}).get('articlesmultistreamdump')
