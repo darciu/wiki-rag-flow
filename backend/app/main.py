9e9d245641d7e135ea770a9958a22c8e1df76b0e
darciu <giemzadariusz@gmail.com>
2026-03-01 20:26:29 +0100

    fast_api module is done

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Request, Depends, HTTPException
+from fastapi import FastAPI, Request, Depends, HTTPException, Response
 from llm.interface import get_chat_response
 from backend.app.schemas import ChatRequest, ChatResponse, FeedbackRequest, FeedbackResponse
 from uuid import uuid4, UUID
@@ -10,8 +10,6 @@ from config import WeaviateSettings
 from openai import OpenAI
 import instructor
 
-InteractionStore = dict[UUID, dict]
-FeedbackStore = list[dict]
 
 
 def create_llm_client():
@@ -62,6 +60,8 @@ async def lifespan(app: FastAPI):
         app.state.llm_client = llm_client
         app.state.weaviate_client = weaviate_client
         app.state.nlp_toolkit = nlp_toolkit
+        app.state.chat_last_session = None
+        app.state.app_run_id = uuid4()
 
         yield
 
@@ -89,8 +89,6 @@ def get_nlp_toolkit(request: Request):
     except AttributeError:
         raise HTTPException(status_code=503, detail="NLP toolkit not initialized")
     
-def get_interactions_store(request: Request) -> InteractionStore:
-    return request.app.state.interactions
 
 
 
@@ -101,6 +99,7 @@ def health():
 
 @app.post("/chat", response_model=ChatResponse)
 def chat(chat_reqest: ChatRequest,
+         request: Request, response: Response,
          llm_client=Depends(get_llm_client),
             weaviate_client=Depends(get_weaviate_client),
             nlp_toolkit=Depends(get_nlp_toolkit),
@@ -116,7 +115,24 @@ def chat(chat_reqest: ChatRequest,
             nlp_toolkit=nlp_toolkit,
             model_name=model_name,
         )
-        chat_response = ChatResponse(answer=answer, suggested_prompts=suggested_questions, route=route_type, id=response_id)
+        
+
+        session_id = request.cookies.get("session_id")
+        if not session_id:
+            session_id = str(uuid4())
+            response.set_cookie(key="session_id", value=session_id, httponly=True)
+
+        chat_response = ChatResponse(answer=answer, suggested_prompts=suggested_questions, route=route_type, id=response_id,
+                                     user_agent = request.headers.get("user-agent"),
+                                     session_id = session_id,
+                                     app_run_id = request.app.state.app_run_id,
+                                     )
+
+        # for feedback
+        request.app.state.chat_last_session = {
+            "chat_response_id": response_id,
+            "chat_route": route_type
+        }
 
         return chat_response
     
@@ -124,6 +140,31 @@ def chat(chat_reqest: ChatRequest,
         raise HTTPException(status_code=500, detail=str(e))
     
 
-# @app.post("/feedback", response_model=FeedbackResponse)
-# def feedback
+@app.post("/feedback", response_model=FeedbackResponse)
+def post_feedback(feedback_request: FeedbackRequest, request: Request):
+
+    chat_last_session = request.app.state.chat_last_session
+    if not chat_last_session:
+        raise HTTPException(
+            status_code=400, 
+            detail="Feedback cannot be submitted. No active chat response found or feedback already sent."
+        )
+
+    feedback_response = FeedbackResponse(
+        rating=feedback_request.rating,
+        chat_route=chat_last_session["chat_route"],
+        chat_response_id=chat_last_session["chat_response_id"],
+        user_agent = request.headers.get("user-agent"),
+        session_id = request.cookies.get("session_id"),
+        app_run_id = request.app.state.app_run_id
+    )
+    request.app.state.chat_last_session = None
+
+    try:
+        pass
+    # save data to DB
+    except:
+        raise HTTPException(status_code=500, detail="Failed to save feedback to database.")
+
+    return feedback_response
  
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 from typing import Literal
 from enum import StrEnum
 from uuid import UUID, uuid4
-from datetime import datetime
+from datetime import datetime, timezone
 
 class RouteType(StrEnum):
     RAG_SEARCH = "RAG_SEARCH"
@@ -13,6 +13,7 @@ class ChatRequest(BaseModel):
     question: str = Field(..., min_length=1)
     model_name: str = "llama3.2"
     id: UUID = Field(default_factory=uuid4, description="Chat request ID")
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 
 class ChatResponse(BaseModel):
@@ -20,15 +21,22 @@ class ChatResponse(BaseModel):
     suggested_prompts: list[str] = Field(default_factory=list, description="Suggest")
     route: RouteType = Field(..., description="LLMs routing")
     id: UUID = Field(default_factory=uuid4, description="Chat response ID")
+    user_agent: str | None = Field(None, description="User data from browser")
+    session_id: str  = Field(..., description="Cookie session id")
+    app_run_id: UUID = Field(..., description="FastAPI run ID")
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 class FeedbackRequest(BaseModel):
     rating: Literal["up", "down"]
 
 class FeedbackResponse(BaseModel):
     rating: Literal["up", "down"]
-    route: RouteType = Field(..., description="LLMs routing")
-    id: UUID = Field(default_factory=uuid4, description="Chat response ID")
-    created_at: datetime
+    chat_route: RouteType = Field(..., description="LLMs routing")
+    chat_response_id: UUID = Field(..., description="Chat response ID")
+    user_agent: str | None = Field(None, description="User data from browser")
+    session_id: str  = Field(..., description="Cookie session id")
+    app_run_id: UUID = Field(..., description="FastAPI run ID")
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 
 
